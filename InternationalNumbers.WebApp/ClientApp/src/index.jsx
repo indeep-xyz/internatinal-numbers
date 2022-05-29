6fccc57ab78f3d40e7daa50902d467feba45f8e0
indeep-xyz <indeep.xyz@gmail.com>
2022-05-29 22:57:30 +0900

    React 18 用に index 系ファイルを更新

--- InternationalNumbers.WebApp/ClientApp/src/index.jsx
+++ InternationalNumbers.WebApp/ClientApp/src/index.jsx
@@ -1,18 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import registerServiceWorker from './helpers/registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
   <BrowserRouter basename={baseUrl}>
     <App />
-  </BrowserRouter>,
-  rootElement);
+  </BrowserRouter>
+);
 
 registerServiceWorker();
 
