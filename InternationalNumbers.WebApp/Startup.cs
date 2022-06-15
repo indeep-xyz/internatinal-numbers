6917fbc358d6ab403ea288cc7c815e4995d77e06
indeep-xyz <indeep.xyz@gmail.com>
2022-06-15 23:50:04 +0900

    サーバー項目の設定

--- InternationalNumbers.WebApp/Startup.cs
+++ InternationalNumbers.WebApp/Startup.cs
@@ -43,6 +43,12 @@ namespace InternationalNumbers.WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor
+                                 | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
